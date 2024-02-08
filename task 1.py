1b3e3ae9e7671206bcc6c16acb4c6303e7fa5b50
Thomas Hallford <si23916@bristol.ac.uk>
2024-02-08 12:49:22 +0000

    task 1.py

--- task 1.py
+++ task 1.py
@@ -9,15 +9,21 @@ import sys
 # results = [process(number) for number in list_of_numbers]
 
 
-def input_handler():
-    return [int(arg) for arg in sys.argv[1:]]
+# def input_handler():
+#     return [int(arg) for arg in sys.argv[1:]]
+#
+#
+# def sum_of_list(nums):
+#     total = 0
+#     sum_of_nums = [total := total + numbers for numbers in nums]
+#     return sum_of_nums[len(sum_of_nums) - 1]
+#
+#
+# list_of_nums = input_handler()
+# print(sum_of_list(list_of_nums))
 
 
-def sum_of_list(nums):
-    total = 0
-    sum_of_nums = [total := total + numbers for numbers in nums]
-    return sum_of_nums[len(sum_of_nums) - 1]
+def is_vowel(character):
+    vowels = "AEIOUaeiou"
+    return character in vowels
 
-
-list_of_nums = input_handler()
-print(sum_of_list(list_of_nums))
