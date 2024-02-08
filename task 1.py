0210b6d2ffd9741c8ccdbdb4d0c6d3a79e4969c5
Thomas Hallford <si23916@bristol.ac.uk>
2024-02-08 12:35:23 +0000

    task 1.py

---
+++ task 1.py
@@ -0,0 +1,23 @@
+import sys
+
+
+# def process(num):
+#     return num * 2
+#
+#
+# list_of_numbers = [1, 2, 3, 4]
+# results = [process(number) for number in list_of_numbers]
+
+
+def input_handler():
+    return [int(arg) for arg in sys.argv[1:]]
+
+
+def sum_of_list(nums):
+    total = 0
+    sum_of_nums = [total := total + numbers for numbers in nums]
+    return sum_of_nums[len(sum_of_nums) - 1]
+
+
+list_of_nums = input_handler()
+print(sum_of_list(list_of_nums))
